64ddb73d836f71e182e7c5860b83987fcf6a6b14
Cristhian Rodrigues <tccrmq121@gmail.com>
2022-05-10 14:06:06 -0300

    10-05-22

--- php/cadastro_estab.php
+++ php/cadastro_estab.php
@@ -12,7 +12,68 @@ if(isset($_FILES['foto_perfil_imovel'])) { /*Verifica se um arquivo foi enviado*
     $extensao = strtolower(pathinfo($nomefoto0, PATHINFO_EXTENSION)); /*Extrair extensão do arquivo*/
     $caminho = $pasta . $nomefoto . "." . $extensao; /*Concatenação gerando o nome do arquivo*/
     move_uploaded_file($foto['tmp_name'], $caminho); /*Salva foto no caminho especificado*/
+    $imagem = $caminho;
+
     
 }
+
+
+
+$cep = $_POST["cep"];
+$endereco = $_POST["endereco"];
+$complemento = $_POST["complemento"];
+$bairro = $_POST["bairro"];
+$cidade = $_POST["cidade"];
+$estado = $_POST["estado"];
+$banheiros = $_POST["banheiros"];
+$tamanho_casa = $_POST["tamanho_casa"];
+$valor_aluguel= $_POST["valor_aluguel"];
+$negociavel = $_POST["negociavel"];
+$apartamento_ou_casa = $_POST["apartamento_ou_casa"];
+$comodos= $_POST["comodos"];
+$quartos= $_POST["quartos"];
+$lavanderia = $_POST["lavanderia"];
+$vagas_carro = $_POST["vagas_carro"];
+$internet = $_POST["internet"];
+/*$imagem*/
+$descricao = $_POST["descricao"];
+$id_locador = $_GET[$last_id];
+
+
+$stmt = mysqli_prepare($mysqli, "INSERT INTO estabelecimentos (CEP, endereco, complemento, bairro, cidade, estado, quartos, tamanho_casa, valor, negociavel, descricao, ap_ou_casa, vagas_carro, comodos, banheiros, lavanderia, internet, id_locador, foto_perfil_casa) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
+ 
+mysqli_stmt_bind_param($stmt, "ssssssiiiisiiiiiiss", $pcep, $pendereco, $pcomplemento, $pbairro, $pcidade, $pestado, $pquartos, $ptamanho_casa, $pvalor_aluguel, $pnegociavel , $pdescricao, $papartamento_ou_casa, $pvagas_carro, $pcomodos, $pbanheiros, $plavanderia, $pinternet, $pidl, $pfoto);
+
+$pcep = $cep;
+$pendereco = $endereco;
+$pcomplemento = $complemento;
+$pbairro = $bairro;
+$pcidade = $cidade;
+$pestado = $estado;
+$pquartos = $quartos;
+$ptamanho_casa = $tamanho_casa;
+$pvalor_aluguel = $pvalor_aluguel;
+$pnegociavel  = $negociavel;
+$pdescricao = $descricao;
+$papartamento_ou_casa = $apartamento_ou_casa;
+$pvagas_carro = $vagas_carro;
+$pcomodos = $comodos;
+$pbanheiros = $banheiros;
+$plavanderia = $lavanderia;
+$pinternet = $internet;
+$pidl = $id_locador;
+$pfoto = $imagem;
+
+/* Execute the statement */
+mysqli_stmt_execute($stmt);
+
+$last_id2 = mysqli_insert_id($mysqli);
+
+header('location: ../Cadastro quarto/index.html?id=$last_id2')
+
+
+
+
+
 
 ?>
