302997c7e4e4627e34ce1ec7a8bfd6dbdcf33e9a
Cristhian Rodrigues <tccrmq121@gmail.com>
2022-05-10 12:58:41 -0300

    09-05-2022

--- php/cadastro_estab.php
+++ php/cadastro_estab.php
@@ -7,26 +7,12 @@ if(isset($_FILES['foto_perfil_imovel'])) { /*Verifica se um arquivo foi enviado*
     $foto = $_FILES['foto_perfil_imovel'];
 
     $pasta = "../userimages/"; /*Pasta onde os arquivos vão ser salvos*/
+    $nomefoto0 = $foto['name'];
     $nomefoto = uniqid(); /*Criar um nome aleatório para o novo arquivo*/
-    $extensao = patchinfo($foto, PATHINFO_EXTENSION); /*Extrair extensão do arquivo*/
+    $extensao = strtolower(pathinfo($nomefoto0, PATHINFO_EXTENSION)); /*Extrair extensão do arquivo*/
     $caminho = $pasta . $nomefoto . "." . $extensao; /*Concatenação gerando o nome do arquivo*/
-
-    move_uploaded_file($foto['tmp_name'], $caminho) /*Salva foto no caminho especificado*/
-
+    move_uploaded_file($foto['tmp_name'], $caminho); /*Salva foto no caminho especificado*/
+    
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 ?>
